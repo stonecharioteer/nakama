fca89a775aea80fd35f513cc9b1a344f68e78ec7
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2017-12-27 19:43:18 +0530

    Avoiding having to write to a bmp.

--- run.py
+++ run.py
@@ -15,16 +15,14 @@ def run():
     img_b, img_r = get_op_stat_image()
     img_b = img_b.transpose(Image.ROTATE_90)
     img_r = img_r.transpose(Image.ROTATE_90)
-    img_b.save("black.bmp")
-    img_r.save("red.bmp")
     epd = epd2in7b.EPD()
     epd.init()
     # # clear the frame buffer
     # frame_black = [0] * (epd.width * epd.height / 8)
     # frame_red = [0] * (epd.width * epd.height / 8)
 
-    frame_black = epd.get_frame_buffer(Image.open('black.bmp'))
-    frame_red = epd.get_frame_buffer(Image.open('red.bmp'))
+    frame_black = epd.get_frame_buffer(img_b)
+    frame_red = epd.get_frame_buffer(img_r)
     epd.display_frame(frame_black, frame_red)
 
     #os.remove("black.bmp")
