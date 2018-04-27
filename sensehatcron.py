3dfa1329b2d76d6b95c1be67e65a4bbfa0f0da37
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2018-04-27 21:43:33 +0530

    Fixed mistake with function call.

--- sensehatcron.py
+++ sensehatcron.py
@@ -7,13 +7,13 @@ from nakama import get_latest_one_piece_chapter_jb, get_latest_one_piece_chapter
 if __name__ == "__main__":
     jaiminis_box = get_latest_one_piece_chapter_jb()
     s = SenseHat()
-    s.set_orientation = 180
+    s.set_orientation(180)
     if jaiminis_box["release_date"] == datetime.date.today():
         for i in range(10):
             s.show_message(
                 "One Piece #{} {} out now on {}.".format(jaiminis_box["chapter_number"], jaiminis_box["chapter_name"], "Jaimini's Box"),
                     text_colour = [255,0,0],
-                    scroll_speed = 0.03)
+                    scroll_speed = 0.01)
     else:
         mangastream = get_latest_one_piece_chapter_ms()
         if mangastream["release_date"] == datetime.date.today():
@@ -21,13 +21,13 @@ if __name__ == "__main__":
                 s.show_message(
                 "One Piece #{} {} out now on {}.".format(mangastream["chapter_number"], mangastream["chapter_name"], "MangaStream"),
                 text_colour = [255,0,0],
-                scroll_speed = 0.03)
+                scroll_speed = 0.01)
         else:
             s.show_message(
                 "No new One Piece yet! :(.    Last chapter was: #{} {} @ {}.".format(mangastream["chapter_number"], mangastream["chapter_name"], "MangaStream"),
                 text_colour = [0,255,0],
-                scroll_speed = 0.03)
+                scroll_speed = 0.01)
 
-    s.set_orientation = 0
+    s.set_orientation(0)
     s.blank()
 
